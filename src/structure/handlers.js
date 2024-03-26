824d051c1f2438f58c164abd18ffff92b0141195
Joshua Allen <batayolajoshuaallen@gmail.com>
2024-03-26 19:01:10 +0800

    added logic for navbar

--- src/structure/handlers.js
+++ src/structure/handlers.js
@@ -7,8 +7,31 @@ export default class Handler {
     const navToggle = document.querySelector(".menu-toggle");
     navToggle.addEventListener("click", () => {
       const navItems = document.querySelector(".nav-container");
-
       navItems.classList.toggle("active");
+    });
+
+    const navLinks = document.querySelectorAll(".nav-links");
+
+    navLinks.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        navLinks.forEach((navLink) => {
+          navLink.classList.remove("highlight");
+        });
+
+        link.classList.add("highlight");
+
+        const targetId = link.getAttribute("href");
+
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - 200, // Adjust the offset as needed
+            behavior: "smooth",
+          });
+        }
+      });
     });
   };
 }
