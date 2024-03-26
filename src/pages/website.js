3610b532031748379fa21d89e24bd8ab5f7fa8b0
Joshua Allen <batayolajoshuaallen@gmail.com>
2024-03-26 18:59:08 +0800

    import work, contact and hero.js

--- src/pages/website.js
+++ src/pages/website.js
@@ -1,15 +1,21 @@
 import header from "../structure/header.js";
 import Handler from "../structure/handlers.js";
+import Hero from "../pages/hero.js";
 import About from "../pages/about.js";
 import Skill from "../pages/skills.js";
+import Work from "../pages/work.js";
+import Contact from "../pages/contact.js";
 
 export default class Webpage {
   static loadWeb = () => {
     const container = document.querySelector(".container");
 
     container.appendChild(header.loadHeader());
+    container.appendChild(Hero.loadHero());
     container.appendChild(About.loadAbout());
     container.appendChild(Skill.loadSkill());
+    container.appendChild(Work.loadWork());
+    container.appendChild(Contact.loadContact());
 
     Handler.loadHandler();
   };
